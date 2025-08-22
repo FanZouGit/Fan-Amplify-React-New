f2165afb9567086a66ce3b4ca485daa6a283a268
EC2 Default User <ec2-user@ip-172-31-41-21.ec2.internal>
2025-08-22 20:58:15 +0000

    add debug

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Amplify } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
-import awsconfig from './aws-exports';
+import awsExports from './aws-exports';
 import Upload from './Upload';
 import '@aws-amplify/ui-react/styles.css';
 
-Amplify.configure(awsconfig);
+console.log('Amplify config:', awsExports); // Debug
+Amplify.configure(awsExports);
 
 function App({ user, signOut }) {
   return (
